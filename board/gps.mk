ab039c2efe823ea1d676bd48f95ae68547a4bbae
Maxim <hqdevnews@gmail.com>
2016-07-27 14:03:14 +0700

    Gps integration #1
    Gps integration #1

--- board/gps.mk
+++ board/gps.mk
@@ -1,3 +1,7 @@
 # GPS
 BOARD_GPS_LIBRARIES :=true
 BOARD_MEDIATEK_USES_GPS := true
+
+# AGPS configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/agps_profiles_conf2.xml:system/etc/agps_profiles_conf2.xml \
