dc8e4b528d3b848c33605ca95dd5a55ee4935a59
Alex Kalles <alexakis1997@gmail.com>
2016-07-28 18:28:00 +0300

    Line up

--- product/camera.mk
+++ product/camera.mk
@@ -4,4 +4,4 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/camera/camerasize.xml:system/etc/camerasize.xml \
-	$(LOCAL_PATH)/configs/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml
+    $(LOCAL_PATH)/configs/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml
