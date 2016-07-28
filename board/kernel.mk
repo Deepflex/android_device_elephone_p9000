edf0dc01f5c17c3f195d14c11e5a4be84ec32ef6
Maxim <hqdevnews@gmail.com>
2016-07-28 13:57:17 +0700

    androidboot.selinux=permissive
    androidboot.selinux=permissive

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,7 +1,7 @@
 # Kernel informations
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --board 1465391499 --ramdisk_offset 0x04f88000 --second_offset 0x00e88000 --tags_offset 0x03f88000
 
 # Kernel properties
