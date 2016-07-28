0a701b6cbbf10b5442a881e88ecac9bc1c253268
Maxim <hqdevnews@gmail.com>
2016-07-28 13:03:59 +0700

    Build 2
    Build 2

--- product/gps.mk
+++ product/gps.mk
@@ -4,4 +4,5 @@ $(call inherit-product, device/common/gps/gps_us_supl.mk)
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/slp_conf:system/etc/slp_conf \
+    $(LOCAL_PATH)/configs/agps_profiles_conf2.xml:system/etc/agps_profiles_conf2.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
