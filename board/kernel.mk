9d35abfb096a36d9a6832d411ffa6d9e0d064c4b
Jonathon Fitch <lightman1@virginmedia.com>
2016-07-28 02:45:34 +0100

    Cleanup device tree #2

--- board/kernel.mk
+++ board/kernel.mk
@@ -5,4 +5,4 @@ BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_MKBOOTIMG_ARGS := --board 1465391499 --ramdisk_offset 0x04f88000 --second_offset 0x00e88000 --tags_offset 0x03f88000
 
 # Kernel properties
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilts/kernel
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilts/zImage
