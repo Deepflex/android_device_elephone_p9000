4dfd27442eb5cb718a04bc11353e939d02524040
Alex Kalles <alexakis1997@gmail.com>
2016-07-29 19:58:48 +0300

    Duplicate
    It exist in product/gps.mk

--- board/gps.mk
+++ board/gps.mk
@@ -1,7 +1,3 @@
 # GPS
 BOARD_GPS_LIBRARIES :=true
 BOARD_MEDIATEK_USES_GPS := true
-
-# AGPS configuration
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/agps_profiles_conf2.xml:system/etc/agps_profiles_conf2.xml \
