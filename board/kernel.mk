cc13e2104df51ca012ac729d4ccfd81be908eac7
Alex Kalles <alexakis1997@gmail.com>
2016-10-25 14:20:47 +0300

    Revert: Enforcing SELinux

--- board/kernel.mk
+++ board/kernel.mk
@@ -2,7 +2,7 @@
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --board 1465391499 --ramdisk_offset 0x04f88000 --second_offset 0x00e88000 --tags_offset 0x03f88000
 
 # Kernel properties
