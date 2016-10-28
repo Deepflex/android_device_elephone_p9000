c680aba8ba704a952a381165a362a4d5fb575ec0
Alex Kalles <alexakis1997@gmail.com>
2016-10-29 00:14:21 +0300

    Update ril.mk

--- product/ril.mk
+++ product/ril.mk
@@ -1,5 +1,5 @@
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.kernel.android.checkjni=0 \
-    ro.telephony.ril_class=MediaTekRIL \
+    ro.telephony.ril_class=MT6755 \
     ro.telephony.ril.config=fakeiccid
